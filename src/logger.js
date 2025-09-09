c2aaea47334e3ce75b0ad29f483272a990b4f3b3
smart box <smartbox.advisors@gmail.com>
2025-09-09 18:41:04 +0530

    Add backend and frontend into dashboard repo

--- src/logger.js
+++
@@ -1,7 +0,0 @@
-import pino from 'pino';
-export const log = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: process.env.NODE_ENV !== 'production'
-    ? { target: 'pino-pretty', options: { translateTime: 'SYS:standard', colorize: true } }
-    : undefined,
-});
